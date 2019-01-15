02ac47df7253ee75849e949d7ee0014af1b39666
yzq <1149635502@qq.com>
2019-01-15 11:21:53 +0800

    更新gradle版本

--- app/src/main/java/com/yzq/kotlincommon/App.kt
+++ app/src/main/java/com/yzq/kotlincommon/App.kt
@@ -1,6 +1,8 @@
 package com.yzq.kotlincommon
 
 import android.content.Context
+import android.os.Debug
+import androidx.core.os.TraceCompat
 import androidx.multidex.MultiDex
 import com.yzq.common.BaseApp
 
@@ -9,6 +11,7 @@ class App : BaseApp() {
 
     override fun onCreate() {
         super.onCreate()
+
     }
 
     override fun attachBaseContext(base: Context?) {
