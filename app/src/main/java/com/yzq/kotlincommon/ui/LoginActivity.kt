a0fb659f3f9575787e2e3dac28a43e44e4ef19db
yzq <1149635502@qq.com>
2019-05-16 16:24:47 +0800

    登录页面隐藏状态栏

--- app/src/main/java/com/yzq/kotlincommon/ui/LoginActivity.kt
+++ app/src/main/java/com/yzq/kotlincommon/ui/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.yzq.kotlincommon.ui
 
 import androidx.appcompat.widget.Toolbar
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.blankj.utilcode.util.BarUtils
 import com.yzq.common.constants.RoutePath
 import com.yzq.common.ui.BaseActivity
 import com.yzq.common.utils.LocalSpUtils
@@ -28,13 +29,12 @@ class LoginActivity : BaseActivity() {
 
     override fun initWidget() {
         super.initWidget()
-       // val toolbar = findViewById<Toolbar>(R.id.toolbar)
 
-        transparentStatusBar()
+        BarUtils.setStatusBarVisibility(this,false)
+
         input_account.setText(LocalSpUtils.account)
         input_pwd.setText(LocalSpUtils.pwd)
 
-       // initToolbar(toolbar, "登录")
 
         btn_login.setOnClickListener {
 
