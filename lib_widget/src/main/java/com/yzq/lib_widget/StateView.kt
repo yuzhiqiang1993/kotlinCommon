954ef2d4bbb6abf87a09b749bb0374c39cb8b72b
yzq1993 <1149635502@qq.com>
2019-10-29 11:11:33 +0800

    StateView新增 无数据、无网络、错误时的显示图片自定义属性

--- lib_widget/src/main/java/com/yzq/lib_widget/StateView.kt
+++ lib_widget/src/main/java/com/yzq/lib_widget/StateView.kt
@@ -20,24 +20,48 @@ import kotlinx.android.synthetic.main.layout_state_view.view.*
 typealias RetryListener = () -> Unit
 
 class StateView(
-    context: Context?,
+    context: Context,
     attrs: AttributeSet?,
     defStyleAttr: Int
 ) :
     LinearLayout(context, attrs, defStyleAttr) {
 
 
-    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context?) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context) : this(context, null)
 
 
-    private var view: View = LayoutInflater.from(context).inflate(R.layout.layout_state_view, this)
+    private var view: View
+
+    /*无网络图片*/
+    private var noNetImgRes = R.drawable.ic_no_net
+    /*无数据图片*/
+    private var noDataImgRes = R.drawable.ic_no_data
+    /*出现错误图片*/
+    private var errorImgRes = R.drawable.ic_error
 
     init {
+        view = LayoutInflater.from(context).inflate(R.layout.layout_state_view, this)
+
+        val typeArr = context.obtainStyledAttributes(attrs, R.styleable.StateView)
+
+        try {
+
+            noNetImgRes =
+                typeArr.getResourceId(R.styleable.StateView_no_net_img_res, R.drawable.ic_no_net)
+            noDataImgRes =
+                typeArr.getResourceId(R.styleable.StateView_no_data_img_res, R.drawable.ic_no_data)
+            errorImgRes =
+                typeArr.getResourceId(R.styleable.StateView_error_img_res, R.drawable.ic_error)
+
+        } finally {
+            typeArr.recycle()
+        }
+
+
 
 
         showLoading()
-
     }
 
 
@@ -55,7 +79,7 @@ class StateView(
         view.visibility = View.VISIBLE
         loadding_layout.visibility = View.GONE
         abnorma_layout.visibility = View.VISIBLE
-        iv_hint.setImageResource(R.drawable.ic_no_data)
+        iv_hint.setImageResource(noDataImgRes)
         tv_hint.text = resources.getString(R.string.no_data)
     }
 
@@ -63,7 +87,7 @@ class StateView(
         view.visibility = View.VISIBLE
         loadding_layout.visibility = View.GONE
         abnorma_layout.visibility = View.VISIBLE
-        iv_hint.setImageResource(R.drawable.ic_no_net)
+        iv_hint.setImageResource(noNetImgRes)
         // ImageLoader.getInstance().load(context, R.drawable.ic_no_net, hintImg);
         tv_hint.text = resources.getString(R.string.no_net)
     }
@@ -73,7 +97,7 @@ class StateView(
         loadding_layout.visibility = View.GONE
         abnorma_layout.visibility = View.VISIBLE
 
-        iv_hint.setImageResource(R.drawable.ic_error)
+        iv_hint.setImageResource(errorImgRes)
 
         tv_hint.text = errorMsg
     }
