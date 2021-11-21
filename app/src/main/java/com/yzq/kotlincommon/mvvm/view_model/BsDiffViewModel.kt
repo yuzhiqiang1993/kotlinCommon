5a837c099dca972dff586403fe524c972ff0d4c4
XeonYu <zhiqiang.yu.xeon@gmail.com>
2021-11-21 20:36:37 +0800

    update bsdiff更改为远程依赖

--- app/src/main/java/com/yzq/kotlincommon/mvvm/view_model/BsDiffViewModel.kt
+++ app/src/main/java/com/yzq/kotlincommon/mvvm/view_model/BsDiffViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import com.blankj.utilcode.util.FileUtils
 import com.blankj.utilcode.util.PathUtils
 import com.blankj.utilcode.util.ToastUtils
-import com.xeon.bsdiff.BsDiffUtil
+import com.xeon.bsdiff.utils.XeonBsDiffUtil
 import com.yzq.lib_base.view_model.BaseViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -24,6 +24,19 @@ class BsDiffViewModel : BaseViewModel() {
     val combineFileMD5LiveData by lazy { MutableLiveData<String>() }
     val suffix = "apk"
 
+    /*新文件*/
+    private val newFile = File(PathUtils.getExternalAppCachePath(), "new.${suffix}")
+
+    /*旧文件*/
+    private val oldFile = File(PathUtils.getExternalAppCachePath(), "old.${suffix}")
+
+    /*合并后的文件*/
+    private val combineFile = File(PathUtils.getExternalAppCachePath(), "combine.${suffix}")
+
+    /*生成的补丁文件*/
+    private val patchFile = File(PathUtils.getExternalAppCachePath(), "patch.${suffix}")
+
+
     /**
      * 生成差分包,非常的耗时且占用内存，一般都是在服务端进行
      */
@@ -35,19 +48,15 @@ class BsDiffViewModel : BaseViewModel() {
                 withContext(Dispatchers.IO) {
 
 
-                    val oldFile = File(PathUtils.getExternalAppCachePath(), "old.${suffix}")
-                    val newFile = File(PathUtils.getExternalAppCachePath(), "new.${suffix}")
-                    val patchFile = File(PathUtils.getExternalAppCachePath(), "patch.${suffix}")
-
                     if (!oldFile.exists() || !newFile.exists()) {
                         ToastUtils.showShort("对比包缺失")
                         return@withContext
                     }
 
-                    val fileDiff = BsDiffUtil.fileDiff(
-                            oldFile.absolutePath,
-                            newFile.absolutePath,
-                            patchFile.absolutePath
+                    val fileDiff = XeonBsDiffUtil.diff(
+                        newFile.absolutePath,
+                        oldFile.absolutePath,
+                        patchFile.absolutePath
                     )
 
                 }
@@ -67,10 +76,6 @@ class BsDiffViewModel : BaseViewModel() {
             val measureTimeMillis = measureTimeMillis {
                 withContext(Dispatchers.IO) {
 
-                    val oldFile = File(PathUtils.getExternalAppCachePath(), "old.${suffix}")
-
-                    val combineFile = File(PathUtils.getExternalAppCachePath(), "combine.${suffix}")
-                    val patchFile = File(PathUtils.getExternalAppCachePath(), "patch.${suffix}")
                     if (!oldFile.exists()) {
                         ToastUtils.showShort("旧文件不存在")
                         return@withContext
@@ -80,21 +85,20 @@ class BsDiffViewModel : BaseViewModel() {
                         return@withContext
                     }
 
-                    BsDiffUtil.fileCombine(
-                            oldFile.absolutePath,
-                            combineFile.absolutePath,
-                            patchFile.absolutePath
+                    XeonBsDiffUtil.patch(
+                        oldFile.absolutePath,
+                        patchFile.absolutePath,
+                        combineFile.absolutePath
+
                     )
 
                 }
 
 
-                val newFilePath = "${PathUtils.getExternalAppCachePath()}/new.${suffix}"
-                val combineFilePath = "${PathUtils.getExternalAppCachePath()}/combine.${suffix}"
                 /*计算md5值*/
 
-                newFileMD5LiveData.value = FileUtils.getFileMD5ToString(newFilePath)
-                combineFileMD5LiveData.value = FileUtils.getFileMD5ToString(combineFilePath)
+                newFileMD5LiveData.value = FileUtils.getFileMD5ToString(newFile)
+                combineFileMD5LiveData.value = FileUtils.getFileMD5ToString(combineFile)
             }
 
             ToastUtils.showLong("差分包合并完成，耗时:${measureTimeMillis}")
