5fb5267b87e3739f373c5ddf36e0ea7e3990a348
喻志强 <zhiqiang.yu.xeon@gmail.com>
2022-10-19 11:32:36 +0800

    update 接入core-splashscreen组件，适配Android 12启动画面

--- app/src/main/java/com/yzq/kotlincommon/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/yzq/kotlincommon/ui/activity/SplashActivity.kt
@@ -2,9 +2,15 @@ package com.yzq.kotlincommon.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.blankj.utilcode.util.LogUtils
 import com.yzq.common.constants.RoutePath
 import com.yzq.kotlincommon.R
 import com.yzq.lib_base.extend.navFinish
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * @description: 闪屏页
@@ -13,14 +19,32 @@ import com.yzq.lib_base.extend.navFinish
  * @time   : 6:25 下午
  */
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), SplashScreen.KeepOnScreenCondition {
+
+    private var notReady = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        /*启动屏*/
+        val splashScreen = installSplashScreen()
+        /*设置保持住当前splash*/
+        splashScreen.setKeepOnScreenCondition(this)
         setContentView(R.layout.activity_splash)
-        navFinish(RoutePath.Main.LOGIN)
 
+        MainScope().launch {
+            LogUtils.i("模拟广告耗时")
+            delay(2000)
+            notReady = false
+            LogUtils.i("跳转")
+            navFinish(RoutePath.Main.LOGIN)
+        }
+
+
+    }
+
+    override fun shouldKeepOnScreen(): Boolean {
+        return notReady
     }
 
 }
