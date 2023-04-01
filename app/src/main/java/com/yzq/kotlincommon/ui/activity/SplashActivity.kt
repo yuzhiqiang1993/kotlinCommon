b58f54ac27b5e659f914e504ce86f985b9d6714a
喻志强 <zhiqiang.yu.xeon@gmail.com>
2023-04-01 13:28:39 +0800

    update 完善splashscreen代码，修复 setOnExitAnimationListener 不回调时启动卡住的问题

--- app/src/main/java/com/yzq/kotlincommon/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/yzq/kotlincommon/ui/activity/SplashActivity.kt
@@ -28,6 +28,11 @@ import java.util.concurrent.atomic.AtomicBoolean
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "SplashActivity"
+        private val handleRouteInvoke = AtomicBoolean(false)
+    }
+
     private val keepOnScreenCondition = AtomicBoolean(true)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +41,7 @@ class SplashActivity : AppCompatActivity() {
 
         /*启动屏*/
         installSplashScreen().apply {
-            /*设置保持住当前splash*/
+            /*设置保持住当前splash 主要用来设置动画执行间隔时间来确保动画能够完整的执行一次*/
             setKeepOnScreenCondition {
                 return@setKeepOnScreenCondition keepOnScreenCondition.get()
             }
@@ -44,33 +49,56 @@ class SplashActivity : AppCompatActivity() {
              * 自己处理动画 在这里面才能加一些自己要显示的ui
              */
             setOnExitAnimationListener {
-                /*Android12的设备上 debug时可能会出现回调不执行的情况，手动杀死app进程然后重新打开即可*/
+                /**
+                 * Android12的设备上 debug时或者被其他应用拉起的打开方式会出现setOnExitAnimationListener回调不执行的情况
+                 *
+                 */
                 LogUtils.i("setOnExitAnimationListener")
                 immersive(
                     Color.WHITE,
                     true
                 )//由于splash的主题执行完毕了，所以会显示App主题色的状态栏（默认主主色调是蓝色）不沉浸式的话看起来很怪
+
                 /*路由*/
-                handleRoute()
+//                handleRoute()
             }
 
         }
 
         MainScope().launch {
             LogUtils.i("可以做一些初始化的逻辑，初始化完成后继续走")
-            delay(200)
+
+            /*解除blockui，正常情况下setOnExitAnimationListener会调用  已知Android12不会被调用*/
             keepOnScreenCondition.compareAndSet(true, false)
+            LogUtils.i("可以执行 setOnExitAnimationListener 了")
+
+            /*
+            * 已知在Android 12 上会有setOnExitAnimationListener可能不会被调用的问题
+            * 但是低版本一些弹窗等ui必须要在 setOnExitAnimationListener 执行后再执行，不然可能会报错
+            * 所以，这里最好延时一下给setOnExitAnimationListener执行的机会
+            * */
+
+            delay(200)
+
+            val handleRouteInovke = handleRouteInvoke.get()
+            LogUtils.i("handleRouteInovke:${handleRouteInovke}")
+            if (!handleRouteInovke) {
+                LogUtils.i("handleRouteInvoke 未执行===")
+                handleRoute()
+            }
         }
-
-
-        /*setContentView可以不要*/
-//        setContentView(R.layout.activity_splash)
 
 
     }
 
 
+    @Synchronized
     private fun handleRoute() {
+        if (handleRouteInvoke.get()) {
+            LogUtils.i("handleRoute 已经执行过了.....")
+            return
+        }
+        handleRouteInvoke.compareAndSet(false, true)
         /*先申请权限*/
         getPermissions(
             Permission.ACCESS_COARSE_LOCATION,
@@ -83,6 +111,7 @@ class SplashActivity : AppCompatActivity() {
         ) {
 
             if (MMKVUtil.appFirstOpen) {
+                LogUtils.i("首次打开:${MMKVUtil.appFirstOpen}")
                 /*首次打开可以弹窗提示同意 隐私政策 */
                 showCallbackDialog("提示", "同意隐私政策，用户协议", positiveCallback = {
                     MMKVUtil.appFirstOpen = false
@@ -100,6 +129,7 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun route() {
+
         if (MMKVUtil.hasLogin) {
             navFinish(RoutePath.Main.MAIN)
         } else {
