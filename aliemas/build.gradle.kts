9795de0eff68963156dc21705b5a42c98a77a01b
喻志强 <zhiqiang.yu.xeon@gmail.com>
2023-06-11 13:56:55 +0800

    update 阿里EMAS依赖改重构

--- aliemas/build.gradle.kts
+++ aliemas/build.gradle.kts
@@ -14,19 +14,19 @@ dependencies {
 
 
     //崩溃分析
-    api("com.aliyun.ams:alicloud-android-ha-crashreporter:1.3.0")
+    api(libs.alicloud.android.ha.crashreporter)
     //移动日志
-    api("com.aliyun.ams:alicloud-android-tlog:1.1.4.5-open")
+    api(libs.alicloud.android.tlog)
     //性能监控
-    api("com.aliyun.ams:alicloud-android-apm:1.1.1.0-open")
+    api(libs.alicloud.android.apm)
     //移动分析  埋点 部分arm64-v8a设备上报错： https://help.aliyun.com/document_detail/65275.html
 //    api 'com.aliyun.ams:alicloud-android-man:1.2.7'
     //推送
-    api("com.aliyun.ams:alicloud-android-push:3.8.3")
+    api(libs.alicloud.android.push)
     //公共依赖库
-    api("com.aliyun.ams:alicloud-android-utdid:2.6.0")
-    api("com.aliyun.ams:alicloud-android-utils:2.0.0")
-    api("com.aliyun.ams:alicloud-android-ut:5.4.5")
-    api("com.aliyun.ams:alicloud-android-beacon:1.0.8")
-    api("com.aliyun.ams:alicloud-android-ha-adapter:1.1.5.5-open")
+    api(libs.alicloud.android.utdid)
+    api(libs.alicloud.android.utils)
+    api(libs.alicloud.android.ut)
+    api(libs.alicloud.android.beacon)
+    api(libs.alicloud.android.ha.adapter)
 }
