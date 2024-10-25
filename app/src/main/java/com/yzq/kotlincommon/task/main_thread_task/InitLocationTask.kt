519b77bbad9ac4c9c6bcccadc3e0cc8e3558bd60
yuzhiqiang <zhiqiang.yu.xeon@gmail.com>
2024-10-25 10:30:06 +0800

    update:定位组件版本升级

--- app/src/main/java/com/yzq/kotlincommon/task/main_thread_task/InitLocationTask.kt
+++ app/src/main/java/com/yzq/kotlincommon/task/main_thread_task/InitLocationTask.kt
@@ -1,6 +1,5 @@
 package com.yzq.kotlincommon.task.main_thread_task
 
-import com.yzq.application.AppManager
 import com.yzq.base.startup.base.MainThreadTask
 import com.yzq.location_manager.LocationManager
 import com.yzq.location_protocol.constant.LocationPlatform
@@ -16,10 +15,8 @@ class InitLocationTask : MainThreadTask() {
 
     private val TAG = javaClass.canonicalName
     override fun taskRun() {
-        /*https://help.aliyun.com/document_detail/195006.html*/
 
         LocationManager.init(
-            AppManager.application,
             true,
             arrayOf(LocationPlatform.GAO_DE),
             LocationPlatform.GAO_DE
